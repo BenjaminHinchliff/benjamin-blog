6664d2bccef89b4c9c3e88f096b73d52e1d0f569
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-02-20 23:15:40 -0800

    build start of navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,13 +1,14 @@
-import { Link } from 'gatsby';
 import * as React from 'react';
+import { Link } from 'gatsby';
+import navStyles from './nav.module.css';
 
 const Nav = () => {
     return (
         <nav>
-            <ul>
-                <li>
-                    <Link to="/blog">
-                        Blog
+            <ul className={navStyles.outer}>
+                <li className={navStyles.inner}>
+                    <Link className={navStyles.link} to="/projects">
+                        Projects
                     </Link>
                 </li>
             </ul>
