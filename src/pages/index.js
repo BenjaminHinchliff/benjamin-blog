e70e7ea4c1d89b6b716882e45f284ffc744e4927
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-02-20 22:56:28 -0800

    add header to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react"
+import Header from "../components/header"
 import Nav from "../components/nav"
 
 const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
+      <Header />
       <Nav />
       <h1>Hello world</h1>
     </main>
