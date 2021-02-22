537a0134fb229cfcf115ce1706fa5f4ffdbecb9a
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-02-21 21:40:52 -0800

    tweak summary style

--- src/components/post-summary.js
+++ src/components/post-summary.js
@@ -1,11 +1,12 @@
 import { Link } from 'gatsby';
 import * as React from 'react';
+import postStyles from './post-summary.module.css';
 
 const PostSummary = ({ title, date, exerpt, slug }) => {
     return (
         <div>
-            <h2><Link to={slug}>{title}</Link></h2>
-            <h3>{date}</h3>
+            <h2 className={postStyles.title}><Link to={slug}>{title}</Link></h2>
+            <h3 className={postStyles.date}>{date}</h3>
             <div dangerouslySetInnerHTML={{ __html: exerpt }} />
         </div>
     );
