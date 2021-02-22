9bbbcd4d5204d56af63e46febbde13929f80971e
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-02-21 21:31:05 -0800

    add projects placeholder

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import Header from '../components/header';
+import Layout from '../components/layout';
 import Nav from '../components/nav';
 
-const ProjectsPage = () => {
+const ProjectsPage = ({ location }) => {
     return (
-        <main>
-            <title>Projects</title>
-            <Header />
-            <Nav to="/">Blog</Nav>
-        </main>
+        <Layout title="Projects" location={location} >
+            <p>All my projects are on my <a href="https://github.com/BenjaminHinchliff/" rel="noopener noreferrer">Github</a></p>
+        </Layout>
     );
 }
 
