9bc1a499a22b2e68e691ce5014719540ec8ffbb5
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-02-21 22:20:06 -0800

    fix minor issues

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import headerStyles from "./header.module.css";
 import order from '../images/order.svg';
+import { Link } from 'gatsby';
 
 const Header = () => {
     return (
         <div className={headerStyles.header}>
-            <h1 className={headerStyles.name}>Benjamin Hinchliff</h1>
+            <h1 className={headerStyles.name}><Link to="/">Benjamin Hinchliff</Link></h1>
             <img src={order} height={100} alt="order" />
         </div>
     );
