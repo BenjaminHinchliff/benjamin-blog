0c51a77567e63d8ec9e31320c060a2938fc0485d
Benjamin Hinchliff <benjamin.hinchliff@gmail.com>
2021-02-23 11:51:58 -0800

    add noopener norefferer to project links

--- src/components/project.js
+++ src/components/project.js
@@ -4,7 +4,11 @@ import projectStyles from './project.module.css';
 const Project = ({name, url, description}) => {
   return (
     <div>
-      <h3 className={projectStyles.link}><a href={url}>{name}</a></h3>
+      <h3 className={projectStyles.link}>
+        <a href={url} rel="noopener noreferrer">
+          {name}
+        </a>
+      </h3>
       <p className={projectStyles.description}>
         {description ?? '(no description)'}
       </p>
